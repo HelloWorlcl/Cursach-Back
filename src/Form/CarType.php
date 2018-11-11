340827a967fa47a637716c261ba4e7580871cfbe
v.pintia <pintya@i.ua>
2018-11-12 01:06:02 +0200

    Add Car and Client Forms

---
+++ src/Form/CarType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use App\Entity\Client;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mark', TextType::class, ['required' => true])
+            ->add('model', TextType::class, ['required' => true])
+            ->add('releaseYear', IntegerType::class)
+            ->add('mileage', IntegerType::class)
+            ->add('engineCapacity', IntegerType::class)
+            ->add('fuel', ChoiceType::class, [
+                'choices' => [
+                    'petrol', 'diesel', 'gas', 'electric', 'hybrid', 'other'
+                ]
+            ])
+            ->add('transmission', ChoiceType::class, [
+                'choices' => [
+                    'manual', 'automatic', 'automated_manual', 'continuously_variable', 'other'
+                ]
+            ])
+            ->add('driveUnit', ChoiceType::class, [
+                'choices' => [
+                    'front-wheel', 'rear', 'four-wheel', 'other'
+                ]
+            ])
+            ->add('owner', EntityType::class, ['class' => Client::class])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Car::class,
+        ]);
+    }
+}
